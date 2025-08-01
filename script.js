21030875b8f6e00b9d1bbce96c5d4afa5a90b1df
Angel Forneiro <nforneirorosas@gmail.com>
2025-08-01 03:01:20 -0300

    Terminar funcionalidad del juego (mejorar a futuro)

--- script.js
+++ script.js
@@ -3,15 +3,11 @@
 var number = document.querySelector(".number"); // Selecciona el signo '?'
 var secret_number = Math.ceil(Math.random() * 20); // Se elige un número secreto random
 var message = document.querySelector(".message"); // Se selecciona el mensaje
-console.log(secret_number);
 var again = document.querySelector("#again"); // Selecciona el botón de reset
 var bg = document.querySelector("body"); // Selecciona el body del documento
 var guess = document.querySelector(".guess"); // Selecciona el input donde ponen el valor
 var score = document.querySelector("#score"); // Selecciona el score
-console.log(score.textContent)
 var highscore = document.querySelector("#highscore"); // Selecciona el máximo score
-console.log(highscore);
-console.log(typeof highscore.textContent);
 var vidas = 17;
 var check = document.querySelector("#check"); // Selecciona el botón para checkear la respuesta
 
@@ -20,26 +16,20 @@ check.addEventListener('click', function() { // Se le asigna el evento del click
     if (user_value == secret_number) {
         number.textContent = secret_number;
         message.textContent = "You got it!";
-        bg.style.backgroundColor = "green";
-        if (Number(score.textContent) < Number(highscore.textContent)) {
+        bg.style.backgroundColor = "#31b808ff";
+        if (!highscore.textContent) {
+            highscore.textContent = vidas;
+        } else if (Number(score.textContent) > highscore.textContent) {
             highscore.textContent = vidas;
         }
     } else if (user_value > secret_number) {
-        vidas -= 1;
+        vidas--;
         score.textContent = vidas;
-        if (vidas <= 0) {
-            message.textContent = "You lose!";
-        } else {
-            message.textContent = "Too high"
-        }
+        vidas <= 0 ? message.textContent = "You lose!": message.textContent = "Too high";
     } else if (user_value < secret_number) {
-        vidas -= 1;
+        vidas--;
         score.textContent = vidas;
-        if (vidas <= 0) {
-            message.textContent = "You lose!";
-        } else {
-            message.textContent = "Too low"
-        }
+        vidas <= 0 ? message.textContent = "You lose!": message.textContent = "Too low";
     };
 });
 again.addEventListener('click', () => {
@@ -48,7 +38,6 @@ again.addEventListener('click', () => {
     score.textContent = vidas;
     number.textContent = "?";
     bg.style.backgroundColor = "var(--color_fondo)";
-    guess.value = 0;
+    guess.value = '';
     secret_number = Math.ceil(Math.random() * 20);
-    console.log(secret_number);
 })
